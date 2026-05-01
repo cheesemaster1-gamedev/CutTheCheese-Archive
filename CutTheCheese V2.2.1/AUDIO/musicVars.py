08fdd18b583849c4a6f85bb3bec3775a077536b7
NoahW <Noah-Williams@scas.nsw.edu.au>
2026-05-01 16:31:21 +1000

    V2.2.1 non-ZIP folder removed

--- CutTheCheese V2.2.1/AUDIO/musicVars.py
+++
@@ -1,6 +0,0 @@
-musPeaceful = 'AUDIO/GameSong4.mp3'
-musEerie = 'AUDIO/UndergroundEerie.mp3'
-musCredits = 'AUDIO/Cheesy Terror Loop.mp3'
-musBoss = 'AUDIO/Cheddar Chase.mp3'
-
-selectedMus = musPeaceful
